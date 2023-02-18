86b37af18dd519dfca3de0be94c2e2057fabae48
Kevin <124086102+Silverkevo@users.noreply.github.com>
2023-02-17 20:03:46 -0600

    Creacion de login
    login entidades

---
+++ Facturacion/Entidades/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        public string CodigoUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Rol { get; set; }
+        public string EstaActivo { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string codigoUsuario, string nombre, string contraseña, string correo, DateTime fechaCreacion, string rol, string estaActivo)
+        {
+            CodigoUsuario = codigoUsuario;
+            Nombre = nombre;
+            Contraseña = contraseña;
+            Correo = correo;
+            FechaCreacion = fechaCreacion;
+            Rol = rol;
+            EstaActivo = estaActivo;
+        }
+    }
+}
---
+++ Facturacion/Vista/Login.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Vista
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.ContraseñaTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.ImagenPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // UsuarioTextBox
+            // 
+            this.UsuarioTextBox.Location = new System.Drawing.Point(110, 26);
+            this.UsuarioTextBox.Name = "UsuarioTextBox";
+            this.UsuarioTextBox.Size = new System.Drawing.Size(132, 21);
+            this.UsuarioTextBox.TabIndex = 1;
+            // 
+            // ContraseñaTextBox
+            // 
+            this.ContraseñaTextBox.Location = new System.Drawing.Point(110, 64);
+            this.ContraseñaTextBox.Name = "ContraseñaTextBox";
+            this.ContraseñaTextBox.Size = new System.Drawing.Size(132, 21);
+            this.ContraseñaTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contraseña";
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelarButton.Image = global::Vista.Properties.Resources.cancelar;
+            this.CancelarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CancelarButton.Location = new System.Drawing.Point(140, 99);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(102, 47);
+            this.CancelarButton.TabIndex = 6;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AceptarButton.Image = global::Vista.Properties.Resources.aceptar;
+            this.AceptarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AceptarButton.Location = new System.Drawing.Point(29, 99);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(96, 47);
+            this.AceptarButton.TabIndex = 3;
+            this.AceptarButton.Text = "Aceptar";
+            this.AceptarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.AceptarButton.UseVisualStyleBackColor = true;
+            // 
+            // ImagenPictureBox
+            // 
+            this.ImagenPictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ImagenPictureBox.Image = global::Vista.Properties.Resources.permission;
+            this.ImagenPictureBox.Location = new System.Drawing.Point(269, 26);
+            this.ImagenPictureBox.Name = "ImagenPictureBox";
+            this.ImagenPictureBox.Size = new System.Drawing.Size(99, 96);
+            this.ImagenPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImagenPictureBox.TabIndex = 2;
+            this.ImagenPictureBox.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.AceptarButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.CancelButton = this.CancelarButton;
+            this.ClientSize = new System.Drawing.Size(390, 158);
+            this.ControlBox = false;
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.ContraseñaTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.ImagenPictureBox);
+            this.Controls.Add(this.UsuarioTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UsuarioTextBox;
+        private System.Windows.Forms.PictureBox ImagenPictureBox;
+        private System.Windows.Forms.Button AceptarButton;
+        private System.Windows.Forms.TextBox ContraseñaTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CancelarButton;
+    }
+}
+
